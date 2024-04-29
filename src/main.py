0f696a5d95f3e80edfcf561048f00da812e5f07a
Blake Wingard <bats23456789@gmail.com>
2024-04-29 13:53:49 -0500

    Initialized project

---
+++ src/main.py
@@ -0,0 +1,12 @@
+"""The main entry point for MazeViewer
+
+This file is the main entry point for the MazeViewer project.
+"""
+
+
+def main():
+    print("Hello world!")
+
+
+if __name__ == "__main__":
+    main()
